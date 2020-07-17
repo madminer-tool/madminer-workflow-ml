bf4c7c5aacb81caafe369bee99c69759eb344e9d
Sinclert Pérez <Sinclert@hotmail.com>
2020-07-17 12:33:06 +0200

    Define shared functions

---
+++ code/shared/steps_logging.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import logging
+
+logger = logging.getLogger()
+
+
+def setup_logger(log_level: str):
+    """
+    Set ups a streaming logger with the specified severity level
+    :param log_level: {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"}
+    """
+
+    log_format = logging.Formatter(
+        fmt="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    log_handler = logging.StreamHandler()
+    log_handler.setFormatter(log_format)
+
+    logger.addHandler(log_handler)
+    logger.setLevel(log_level)
