99a7c2495b54ed88c9570201e19efbacc8f57a84
irinaespejo <iem244@nyu.edu>
2020-11-07 21:46:11 -0500

    added QuadraticMorphingAwareRatioEstimator option and propagated change upstream

--- code/train.py
+++ code/train.py
@@ -4,7 +4,10 @@ import argparse
 import mlflow.pytorch
 import os
 
-from madminer.ml import ParameterizedRatioEstimator
+from madminer.ml import (
+    ParameterizedRatioEstimator,
+    QuadraticMorphingAwareRatioEstimator,
+)
 from madminer.ml import ScoreEstimator
 
 
@@ -19,6 +22,7 @@ parser.add_argument("--alpha", type=float)
 parser.add_argument("--batch_size", type=int)
 parser.add_argument("--num_epochs", type=int)
 parser.add_argument("--valid_split", type=float)
+parser.add_argument("--quadratic_classifier", type=bool)
 
 args = parser.parse_args()
 output_dir = args.output_path
@@ -27,7 +31,7 @@ alpha = args.alpha
 batch_size = args.batch_size
 num_epochs = args.num_epochs
 valid_split = args.valid_split
-
+quadratic_classifier = args.quadratic_classifier
 
 #############################
 ### Configuration parsing ###
@@ -37,25 +41,28 @@ path_split = os.path.split(os.path.abspath(samples_path))
 sub_folder = path_split[1]
 
 method = str(sub_folder.split("_", 3)[1])
-models_dir = f'{output_dir}/models'
+models_dir = f"{output_dir}/models"
 
 
 ############################
 ##### Perform training #####
 ############################
 
-ratio_estimator_methods = {'alice', 'alices', 'cascal', 'carl', 'rolr', 'rascal'}
-score_estimator_methods = {'sally', 'sallino'}
+ratio_estimator_methods = {"alice", "alices", "cascal", "carl", "rolr", "rascal"}
+score_estimator_methods = {"sally", "sallino"}
 
 if method in ratio_estimator_methods:
-    estimator = ParameterizedRatioEstimator(n_hidden=(100, 100, 100))
+    if quadratic_classifier:
+        estimator = QuadraticMorphingAwareRatioEstimator(n_hidden=(100, 100, 100))
+    else:
+        estimator = ParameterizedRatioEstimator(n_hidden=(100, 100, 100))
     estimator.train(
         method=method,
-        x=f'{samples_path}/x_{method}_train.npy',
-        y=f'{samples_path}/y_{method}_train.npy',
-        theta=f'{samples_path}/theta0_{method}_train.npy',
-        r_xz=f'{samples_path}/r_xz_{method}_train.npy',
-        t_xz=f'{samples_path}/t_xz_{method}_train.npy',
+        x=f"{samples_path}/x_{method}_train.npy",
+        y=f"{samples_path}/y_{method}_train.npy",
+        theta=f"{samples_path}/theta0_{method}_train.npy",
+        r_xz=f"{samples_path}/r_xz_{method}_train.npy",
+        t_xz=f"{samples_path}/t_xz_{method}_train.npy",
         alpha=alpha,
         n_epochs=num_epochs,
         batch_size=batch_size,
@@ -66,12 +73,12 @@ elif method in score_estimator_methods:
     estimator = ScoreEstimator()
     estimator.train(
         method=method,
-        x=f'{samples_path}/x_{method}_train.npy',
-        t_xz=f'{samples_path}/t_xz_{method}_train.npy',
+        x=f"{samples_path}/x_{method}_train.npy",
+        t_xz=f"{samples_path}/t_xz_{method}_train.npy",
     )
 
 else:
-    raise ValueError('Invalid training method')
+    raise ValueError("Invalid training method")
 
 
 ############################
@@ -79,11 +86,11 @@ else:
 ############################
 
 model_folder_name = method
-model_folder_path = f'{models_dir}/{model_folder_name}'
+model_folder_path = f"{models_dir}/{model_folder_name}"
 os.makedirs(model_folder_path, exist_ok=True)
 
 model_file_name = method
-model_file_path = f'{model_folder_path}/{model_file_name}'
+model_file_path = f"{model_folder_path}/{model_file_name}"
 estimator.save(model_file_path)
 
 
@@ -91,10 +98,9 @@ estimator.save(model_file_path)
 ## MLFlow tracking information ##
 #################################
 
-mlflow.set_tags({
-    "context": "workflow",
-    "method": method,
-})
+mlflow.set_tags(
+    {"context": "workflow", "method": method,}
+)
 
 mlflow.log_artifacts(model_folder_path)
 mlflow.pytorch.log_model(estimator.model, method)
